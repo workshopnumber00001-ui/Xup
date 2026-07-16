0190db10e68e45c244923af4eb2377184827f658
workshopnumber00001-ui <workshopnumber00001@gmail.com>
2026-07-16 21:14:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,12 +48,12 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 5840594311 # Replace with the actual owner's user ID
+OWNER_ID = 5349573682 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5840594311]
+SUDO_USERS = [5349573682]
 
-AUTH_CHANNEL = -1002595188554
+AUTH_CHANNEL = -1002595188
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
